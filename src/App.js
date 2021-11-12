5680c1524701e073db60a861dd4c3fcf015456d4
anasanmua <86674956+anasanmua@users.noreply.github.com>
2021-11-12 11:16:40 +0100

    w5&Forecast

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,86 +1,71 @@
 import React, { useState } from "react";
-import "./Weather.css";
+import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
+import "./Weather.css";
 
-export default function Weather() {
-  const [weatherData, setweatherData] = useState({ ready: false });
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
   function handleResponse(response) {
-    console.log(response.data);
-    setweatherData({
+    setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
-      date: "Wednesday,09",
+      date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+      icon: response.data.weather[0].icon,
       wind: response.data.wind.speed,
       city: response.data.name,
     });
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (weatherData.ready) {
     return (
-      <div>
-        <div className="Weather">
-          <form>
-            <div className="row one">
-              <div className="col-9">
-                <input
-                  type="Search"
-                  placeholder="Insert city"
-                  className="form-control"
-                  autoFocus="on"
-                />
-              </div>
-              <div className="col-3">
-                <input
-                  type="submit"
-                  value="Search"
-                  className="btn btn-primary button"
-                />
-              </div>
-            </div>
-          </form>
-          <h1>{weatherData.city}</h1>
-          <h5> {weatherData.date}</h5>
-          <div className="row one">
-            <div className="col-6">
-              <img
-                src={weatherData.iconUrl}
-                alt={weatherData.description}
-                className="sun-icon"
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
               />
-              <div className="temperature">
-                {Math.round(weatherData.temperature)}{" "}
-              </div>
-              <div className="units">ºF | ºC</div>
             </div>
-
-            <div className="col-6">
-              <ul>
-                <li>Precip :87%</li>
-                <li>Humidity :{weatherData.humidity}</li>
-                <li>Wind :{weatherData.wind}</li>
-              </ul>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
-        </div>
-        <div className="credit">
-          <a
-            className="link"
-            href="https://github.com/anasanmua/react-weather-app0.1"
-          >
-            {" "}
-            Open source{" "}
-          </a>{" "}
-          Coded by Ana S.M.
-        </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
-    const apiKey = "2a01e7be0e91da230ebdfdb345e57960";
-    let city = "New York";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
+    return "Loading...";
   }
-  return "Loading...";
 }
